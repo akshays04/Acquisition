b26f524619f8e4a15f70a2e4ff7bee9ab5fc0a9f
shreyasDarwhatkar <shreyas.darwhatkar@hotmail.com>
2015-05-24 02:56:25 -0700

    SD for australia

--- lab1/standardDeviation.py
+++ lab1/standardDeviation.py
@@ -5,18 +5,18 @@ client = pymongo.MongoClient("localhost", 27017)
 db = client.CricStats
 count = 0
 sum=0
-for each in db.odi.find({ 'location': { '$in': ['Abu Dhabi' ] } }):
+for each in db.odi.find({ 'location': { '$in': ['Adelaide' ] } }):
     sum=sum+int(each.get('team1 Total'))
     count=count+1    
 team1AvgScore=math.ceil(sum/count)
 
 count = 0
 sum=0
-for each in db.odi.find({ 'location': { '$in': ['Abu Dhabi' ] } }):
-    if (each.get('team1')=='Pakistan'):
+for each in db.odi.find({ 'location': { '$in': ['Adelaide' ] } }):
+    if (each.get('team1')=='Australia'):
         sum=sum + int(each.get('team1 Total'))
         count=count+1
-    elif(each.get('team2')=='Pakistan'):
+    elif(each.get('team2')=='Australia'):
         sum=sum +int(each.get('team2 Total'))
         count=count+1
 
@@ -24,7 +24,7 @@ teamAvgScore=math.ceil(sum/count)
 
 standardDeviation=team1AvgScore-teamAvgScore
 
-print 'Standard Deviation for team Pakistan is ' +str(standardDeviation)        
+print 'Standard Deviation for team Australia is ' +str(standardDeviation)        
                               
 #print teamAvgScore
 #print team1AvgScore
